c51e7197110f0ce34611338c1018942c74d8f767
Mostafa Abdelrahman <masaa.5763@gmail.com>
2024-12-18 15:23:14 +0200

    make the methods dynamic

--- src/TechcellSocialite.php
+++ src/TechcellSocialite.php
@@ -2,6 +2,7 @@
 
 namespace TechCell\TechcellSocialite;
 
+use Exception;
 use Laravel\Socialite\Facades\Socialite;
 
 class TechcellSocialite
@@ -10,16 +11,31 @@ class TechcellSocialite
         'google' => 'google',
         'github' => 'github',
         'twitter' => 'twitter-oauth-2',
+        'x' => 'twitter-oauth-2',
         'linkedin' => 'linkedIn',
     ];
 
     public static function loginUsingProvider($provider)
     {
-        return Socialite::driver(static::PROVIDERS[$provider])->redirect();
+        if (!array_key_exists($provider, self::PROVIDERS)) {
+            throw new Exception("Provider $provider not found.");
+        }
+        try {
+            return Socialite::driver(static::PROVIDERS[$provider])->redirect();
+        } catch (Exception $e) {
+            throw new Exception('Authentication Failed.', $e->getMessage());
+        }
     }
 
     public static function callbackFromProvider($provider)
     {
-        return Socialite::driver(static::PROVIDERS[$provider])->user();
+        if (!array_key_exists($provider, self::PROVIDERS)) {
+            throw new Exception("Provider $provider not found.");
+        }
+        try {
+            return Socialite::driver(static::PROVIDERS[$provider])->user();
+        } catch (Exception $e) {
+            throw new Exception('Authentication Failed.');
+        }
     }
 }
